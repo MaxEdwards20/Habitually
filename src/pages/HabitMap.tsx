93d4691298540280b5424018829253548760bf70
medwards <maxedwards20@gmail.com>
2023-04-26 21:53:23 -0600

    Adds todos for habitmap

--- src/pages/HabitMap.tsx
+++ src/pages/HabitMap.tsx
@@ -1,5 +1,7 @@
 import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
 import React, { useEffect, useState } from "react";
+import { loadHabits } from "../utils/hooks";
+import { Habit, HabitLog } from "../utils/models";
 
 type HabitMapProps = {
   lat: number;
@@ -7,22 +9,29 @@ type HabitMapProps = {
 };
 export const HabitMap = ({ lat, long }: HabitMapProps) => {
   const [mapCenter, setMapCenter] = useState({ lat: lat, lng: long });
+  const [habits, setHabits] = useState<Habit[]>([]);
 
   useEffect(() => {
     setMapCenter({ lat: lat, lng: long });
-    console.log("API KEY IS: ", import.meta.env.MAPS_API_KEY);
   }, [lat, long]);
+
+  useEffect(() => {
+    loadHabits().then((habits) => setHabits(habits));
+  }, []);
 
   const mapStyles = {
     height: "400px",
     width: "100%",
   };
+  //TODO: Get all habitLogs for each habit and add a marker for each habitLog
 
   return (
-    <LoadScript googleMapsApiKey={import.meta.env.MAPS_API_KEY}>
-      <GoogleMap mapContainerStyle={mapStyles} center={mapCenter} zoom={14}>
-        <Marker position={mapCenter} />
-      </GoogleMap>
+    <LoadScript googleMapsApiKey={import.meta.env.VITE_MAPS_API_KEY}>
+      <GoogleMap
+        mapContainerStyle={mapStyles}
+        center={mapCenter}
+        zoom={14}
+      ></GoogleMap>
     </LoadScript>
   );
 };
